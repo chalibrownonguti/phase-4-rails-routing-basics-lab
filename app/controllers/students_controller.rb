03bda449d8b7b0d69d0d8806794eb1b9fc3614af
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-06-21 14:03:23 +0300

    update

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,9 +3,15 @@ class StudentsController < ApplicationController
         students = Student.all
     render json: students
     end
+
     def grades
         students = Student.order(grade: :desc)
         render json: students
       end
       
+      def highest_grade
+        student = Student.order(grade: :desc).first
+        render json: student
+      end
+            
 end
